5dc52c3c5ad21f5f06adb7e0812f50ea62b71f8f
Sanjay Bopche <sanjay.bopche@globallogic.com>
2026-09-03 13:26:15 +0530

    Removed mocroservices

--- src/MicroServices/AuthService/Services/Auth/ITokenService.cs
+++
@@ -1,8 +0,0 @@
-namespace AuthService.Services.Auth
-{
-    public interface ITokenService
-    {
-        Task<string> GenerateToken(string username, string email, string role);
-        Task<string> GenerateRefreshToken();
-    }
-}
--- src/MicroServices/AuthService/Services/Users/IUserService.cs
+++
@@ -1,9 +0,0 @@
-using AuthService.Models;
-
-namespace AuthService.Services.Users;
-public interface IUserService
-{
-    Task<AuthResponse> LoginAsync(LoginRequest loginRequest);
-    Task<AuthResponse> RegisterAsync(RegisterRequest registerRequest);
-    Task<AuthResponse> RefreshTokenAsync();
-}
